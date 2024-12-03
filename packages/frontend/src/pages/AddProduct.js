9f8cd05146ce4a336f025a99ce6773203d67ecf6
Alexandre GRASTEAU <88926501+agrasteau@users.noreply.github.com>
2024-12-03 15:06:03 +0100

    fix accessibility buttons colors

--- packages/frontend/src/pages/AddProduct.js
+++ packages/frontend/src/pages/AddProduct.js
@@ -111,7 +111,7 @@ const AddProduct = () => {
             style={{
               flex: 1,
               padding: '10px',
-              backgroundColor: '#4CAF50',
+              backgroundColor: '#4c8250',
               color: 'white',
               border: 'none',
               borderRadius: '4px',
